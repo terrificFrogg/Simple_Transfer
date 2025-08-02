79abe59ccf70a95433c93c063408be52b4e7b1a0
terrificFrogg <sinaye007@outlook.com>
2025-08-02 13:37:04 +0200

    Initial commit

---
+++ src/main/java/org/simpletransfer/Main.java
@@ -0,0 +1,11 @@
+package org.simpletransfer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
